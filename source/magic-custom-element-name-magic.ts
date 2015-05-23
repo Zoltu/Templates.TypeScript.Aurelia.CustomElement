ccde6f1b6842def5545fb3139e9eb537328c6040
Micah Zoltu <micah@zoltu.net>
2015-05-22 22:33:22 -0700

    Adds hello world to custom element.
    This makes it more obvious that everything is working out of the box.

--- source/magic-custom-element-name-magic.ts
+++ source/magic-custom-element-name-magic.ts
@@ -4,4 +4,5 @@ import './magic-custom-element-name-magic.css!';
 @customElement('magic-custom-element-name-magic')
 @useView('./magic-custom-element-name-magic.html')
 export class magic_CustomElementName_magic {
+	heading: string = "Hello world!";
 }
